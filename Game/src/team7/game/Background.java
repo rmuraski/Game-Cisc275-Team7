e74fb33ccee9929399951214c951828e9a6ceacd
Qichao Hong <qichao@udel.edu>
2015-11-03 18:56:59 -0500

    improve the looking

--- Game/src/team7/game/Background.java
+++ Game/src/team7/game/Background.java
@@ -1,8 +1,14 @@
-/**Background class is for deciding the arrangement of the background. */
+/**
+ * Background class is for deciding the arrangement 
+ * of the background. 
+ */
 package team7.game;
 
 public class Background {
-	/**For deciding if objects should be drawn on land or drawn in the water. */
+	/**
+	 * For deciding if objects should be drawn 
+	 * on land or drawn in the water. 
+	 */
 	boolean landArea;
 	boolean waterArea;
 
