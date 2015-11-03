dd8bea74e69aa565f4a533e3996940c10d98cd0e
Qichao Hong <qichao@udel.edu>
2015-11-03 18:55:36 -0500

    Edited Solution Class

--- Game/src/team7/game/Solution.java
+++ Game/src/team7/game/Solution.java
@@ -7,12 +7,24 @@ import java.awt.Point;
  */
 public abstract class Solution {
 	int movingSpeed;
-	boolean isFixing;
+	boolean Fixing=false;
 	int fixingSpeed;
 	Point Coordinates;
 	Events type;
 	
+	public int getMovingSpeed(){
+		return this.movingSpeed;
+	}
+	public boolean isFixing(){
+		return this.Fixing;
+	}
 	public abstract void fix();
+	public int getFixingSpeed(){
+		return this.fixingSpeed;
+	}
+	public Point getCoordinates(){
+		return this.Coordinates;
+	}
 	public int calculateFixTime(){
 		
 		//TODO claculate the fix time return it
