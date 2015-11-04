954db40a3b632969ca8f0e814eaafbb454bf3187
Qichao Hong <qichao@udel.edu>
2015-11-03 19:20:36 -0500

    Edited User class

--- Game/src/team7/game/User.java
+++ Game/src/team7/game/User.java
@@ -4,5 +4,28 @@ public class User {
 	String name;
 	int score;
 	boolean highScore;
-
+	
+	public boolean isHighScore(){
+		return highScore;
+	}
+	public int getScore(){
+		return score;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setScore(int score){
+		this.score=score;
+	}
+	public void setHighScore(boolean isHighScore){
+		this.highScore=isHighScore;
+	}
+	User(String name){
+		setName(name);
+		setScore(0);
+		setHighScore(false);
+	}
 }
