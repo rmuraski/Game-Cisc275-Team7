a03918b3199de8c5f256537cbe034d362ba577f9
Qichao Hong <qichao@udel.edu>
2015-11-09 05:24:11 -0500

    1

--- Game/src/team7/game/GameUI.java
+++ Game/src/team7/game/GameUI.java
@@ -1,70 +1,29 @@
 package team7.game;
-/**GameUI class generates all of the images that are shown on the display.
- *This UI is using Swing animation to generate the images. */
-import java.awt.EventQueue;
+import java.awt.Canvas;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JButton;
-import team7.game.Pollution;
 
-/**
- * @author k
- *
- */
-public class GameUI {
-
-	private JFrame frame;
+public class GameUI extends Canvas{
 
 	/**
-	 * Launch the application.
+	 * 
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GameUI window = new GameUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public GameUI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Mouse Clicked at" + e.getX() + ", " + e.getY());
-			}
-		});
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+	private static final long serialVersionUID = -2953728659860561832L;
+	
+	public GameUI(int width, int height, String title, Game game){
+		JFrame frame=new JFrame(title);
 		
-		JButton btnPollution = new JButton("Pollution");
-		btnPollution.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Pollution p = new Pollution();
-				p.setFixing(true);
-			}
-		});
-		btnPollution.setBounds(42, 41, 117, 29);
-		frame.getContentPane().add(btnPollution);
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
 	}
-
+	
 }
---
+++ Game/src/team7/game/ID.java
@@ -0,0 +1,7 @@
+package team7.game;
+
+public enum ID {
+	Player(),
+	Trash(),
+	Crab();
+}
